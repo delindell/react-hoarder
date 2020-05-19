fc9d96df1a19bc37e60a6d1b95f8314d461b3c66
delindell <daviselindell@gmail.com>
2020-05-18 21:38:31 -0500

    can log in and out

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
-      <h1>inside auth component</h1>
+      <div className="Auth">
+        <button className="btn btn-danger" onClick={this.loginClickEvent}>Google Login</button>
+      </div>
     );
   }
 }
