3c2dc84efe46cc788aa330bbf31de91c90f257e7
delindell <daviselindell@gmail.com>
2020-06-06 13:46:52 -0500

    navBar setup with routing

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import fbConnection from '../helpers/data/connection';
 
-import Auth from '../components/Auth/Auth';
-import MyNavBar from '../components/MyNavBar/MyNavBar';
-import Home from '../components/Home/Home';
+import Auth from '../components/pages/Auth/Auth';
+import MyNavBar from '../components/shared/MyNavBar/MyNavBar';
+import Home from '../components/pages/Home/Home';
+import MyStuff from '../components/shared/MyStuff/MyStuff';
+import New from '../components/pages/New/New';
+import Edit from '../components/pages/Edit/Edit';
+import SingleStuff from '../components/pages/SingleStuff/SingleStuff';
 
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -33,21 +58,28 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      let componentToLoad = '';
-      if (authed) {
-        componentToLoad = <Home />;
-      } else {
-        componentToLoad = <Auth />;
-      }
-      return componentToLoad;
-    };
 
     return (
       <div className="App">
-        <MyNavBar authed={authed}/>
-        <h1>React Hoarding</h1>
-        {loadComponent()}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavBar authed={authed}/>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PrivateRoute path='/home' component={Home} authed={authed} />
+                  <PrivateRoute path='/stuff' component={MyStuff} authed={authed} />
+                  <PrivateRoute path='/new' component={New} authed={authed} />
+                  <PrivateRoute path='/edit' component={Edit} authed={authed} />
+                  <PrivateRoute path='/single' component={SingleStuff} authed={authed} />
+
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <Redirect from="*" to="/home" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/shared/MyStuff/MyStuff.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './MyStuff.scss';
+
+class MyStuff extends React.Component {
+  render() {
+    const editLink = '/edit/12345';
+    const singleLink = '/single/15648';
+    return (
+      <div className="MyStuff">
+        <h1>My Stuff</h1>
+        <Link to={editLink} className="btn btn-outline-warning mr-1">Edit</Link>
+        <Link to={singleLink} className="btn btn-outline-danger">Single</Link>
+      </div>
+    );
+  }
+}
+
+export default MyStuff;
